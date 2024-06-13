dbc73caad591f3db234b17968f9f5be7b7b5218e
PritRaj1 <pr478@cam.ac.uk>
2024-06-13 22:36:12 +0100

    reset KAN layers to how they were

--- KAN/KAN_layers.jl
+++ KAN/KAN_layers.jl
@@ -8,7 +8,7 @@ include("./wavelets/meyer.jl")
 
 export KANdense
 
-using Flux, CUDA, KernelAbstractions, Tullio
+using Flux, CUDA, KernelAbstractions
 using Flux: Dense, BatchNorm
 using NNlib
 using .MexicanHat: MexicanHatWavelet
@@ -37,38 +37,24 @@ wavelet_mapping = Dict(
     "Meyer" => MeyerWavelet
 )
 
-# RNO takes 1D inputs, Transformer takes 2D inputs
-function scale_translate_1D(x, scale, translation)
-    return @tullio out[i, o, b] := (x[i, o, b] - translation[i, o]) / scale[i, o]
-end
-
-function scale_translate_2D(x, scale, translation)
-    return @tullio out[i, o, l, b] := (x[i, o, l, b] - translation[i, o]) / scale[i, o]
-end
-
-function reshape_1D(x, out_size)
-    return repeat(reshape(x, size(x, 1), 1, size(x, 2)), 1, out_size, 1)
-end
-
-function reshape_2D(x, out_size)
-    return repeat(reshape(x, size(x, 1), 1, size(x, 2), size(x, 3)), 1, out_size, 1, 1)
-end
-
-reshape_fcn = bool_2D ? reshape_2D : reshape_1D
-norm_permute = bool_2D ? x -> reshape(x, size(x, 2), size(x, 1), size(x, 3)) : x -> x
-scale_translate_fcn = bool_2D ? scale_translate_2D : scale_translate_1D
-
 struct KANdense_layer
     transform
     output_layer
     batch_norm
     scale
     translation
+    reshape_fcn
+    norm_permute
 end
 
 function KANdense(input_size, output_size, wavelet_name, base_activation, batch_norm)
     wavelet_weights = Flux.kaiming_uniform(input_size, output_size)
-    wavelet = wavelet_mapping[wavelet_name](wavelet_weights)
+    if wavelet_name == "Morlet"
+        args = [5.0]
+    else
+        args = []
+    end
+    wavelet = wavelet_mapping[wavelet_name](args..., wavelet_weights)
     activation = act_mapping[base_activation]
     # output_layer = Flux.Dense(input_size, output_size, activation)
     output_layer = nothing
@@ -77,19 +63,26 @@ function KANdense(input_size, output_size, wavelet_name, base_activation, batch_
     translation = zeros(input_size, output_size)
     scale = ones(input_size, output_size)
 
-    return KANdense_layer(wavelet, output_layer, batch_norm_layer, scale, translation)
+    # RNO takes 1D input, else transformer uses 2D input
+    reshape_fcn = bool_2D ? x -> repeat(reshape(x, size(x, 1), 1, size(x, 2), size(x, 3)), 1, size(translation, 2), 1, 1) : x -> repeat(reshape(x, size(x, 1), 1, size(x, 2)), 1, size(translation, 2), 1)
+    norm_permute = bool_2D ? x -> reshape(x, size(x, 2), size(x, 1), size(x, 3)) : x -> x
+
+    return KANdense_layer(wavelet, output_layer, batch_norm_layer, scale, translation, reshape_fcn, norm_permute)
 end
 
 function (l::KANdense_layer)(x) 
 
-    x_expanded = reshape_fcn(x, size(l.translation, 2))
-    x_expanded = scale_translate_fcn(x_expanded, l.scale, l.translation)
+    x_expanded = l.reshape_fcn(x)
+    translation_expanded = repeat(l.translation, 1, 1, size(x_expanded)[3:end]...)
+    scale_expanded = repeat(l.scale, 1, 1, size(x_expanded)[3:end]...)
+
+    x_expanded = (x_expanded - translation_expanded) ./ scale_expanded 
 
     y = l.transform(x_expanded)
     #z = l.output_layer(x)
     out = y #+ z
-    out = l.batch_norm(norm_permute(out))
-    return norm_permute(out)
+    out = l.batch_norm(l.norm_permute(out))
+    return l.norm_permute(out)
 end
 
 Flux.@functor KANdense_layer

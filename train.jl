d431dd9fa0552b4b01d6def6a85c79401521b9f6
PritRaj1 <pr478@cam.ac.uk>
2024-06-20 13:40:00 +0100

    trained vanilla transformer

--- train.jl
+++ train.jl
@@ -23,7 +23,7 @@ using .hyperparams: set_hyperparams
 
 NUM_REPETITIONS = 5
 
-model_name = "KAN_RNO"
+model_name = "KAN_Transformer"
 hparams = set_hyperparams(model_name)
 batch_size = parse(Int, get(ENV, "batch_size", "32"))
 learning_rate = parse(Float32, get(ENV, "LR", "1e-3"))
